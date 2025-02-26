cf0d504e9ebca5105717c862b87fe4917cd33f38
Rodrigo Bernardo Medeiros <rodrigobernardo.medeiros@halliburton.com>
2025-02-25 23:11:31 -0300

    feat: start ReAct implementation from scratch

---
+++ ice_breaker/react_from_scratch.py
@@ -0,0 +1,7 @@
+def get_text_length(text: str) -> int:
+    """Returns the length of a text by characters"""
+    return len(text)
+
+if __name__ == "__main__":
+    print("Hello ReAct LangChain!")
+    print(get_text_length(text="Dog"))
